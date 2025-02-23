1234c57c1089a7cfa1231931e64020c513ffeff6
Akito UEHARA <akt.uehr@gmail.com>
2025-02-23 13:45:17 +0900

    feat: Footerコンポーネントの実装

---
+++ src/app/features/Footer.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Footer() {
+  return (
+    <footer className="bg-secondary text-primary text-white py-2">
+      <div className="container mx-auto">
+        <p className="text-center">© 2019- パン工房SAKAGUCHI</p>
+      </div>
+    </footer>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { M_PLUS_1p } from "next/font/google";
 import "./globals.css";
 import Header from '@/app/features/Header';
+import Footer from "@/app/features/Footer";
 
 const mPlus1p = M_PLUS_1p({ weight: ['400', '700'], subsets: ['latin'], variable: '--font-m-plus-1p'});
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={mPlus1p.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
