d9ddf92ae8ecd83fd138ee67dca6c4692add149b
Akito UEHARA <akt.uehr@gmail.com>
2025-02-24 13:47:54 +0900

    fix: スタイルの調整

--- src/app/features/BusinessHourInfoSection/components/BusinessHourInfo.tsx
+++ src/app/features/BusinessHourInfoSection/components/BusinessHourInfo.tsx
@@ -8,8 +8,8 @@ type BusinessHourInfoProps = {
 export default function BusinessHourInfo({ title, content }: BusinessHourInfoProps) {
   return (
     <div className="flex gap-x-2 items-center">
-      <span className="py-1 px-2 bg-secondary font-bold text-primary rounded-md">{title}</span>
-      <span className="text-secondary">{content}</span>
+      <span className="py-1 px-2 bg-secondary font-bold text-primary rounded-md text-sm">{title}</span>
+      <span className="text-secondary text-lg">{content}</span>
     </div>
   );
 }
