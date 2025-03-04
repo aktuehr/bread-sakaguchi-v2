67c59b8902315f52619eaa9afcfe58c5dada3f40
Akito UEHARA <akt.uehr@gmail.com>
2025-03-04 14:44:09 +0900

    feat: BusinessHourInfoSectionの背景を調整

--- src/app/features/BusinessHourInfoSection/index.tsx
+++ src/app/features/BusinessHourInfoSection/index.tsx
@@ -4,7 +4,7 @@ import BusinessHourInfo from "./components/BusinessHourInfo";
 
 export default function BusinessHourInfoSection() {
   return (
-    <div className="flex gap-4 justify-center py-1">
+    <div className="flex gap-4 justify-center py-1 bg-primary">
       <BusinessHourInfo title="営業" content="6:00-16:00" />
       <BusinessHourInfo title="定休日" content="水・木" />
     </div>
