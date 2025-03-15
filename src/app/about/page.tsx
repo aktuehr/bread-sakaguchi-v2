bc22b6bd14c487dea3a0becd9998a5872fef6c6f
Akito UEHARA <akt.uehr@gmail.com>
2025-03-15 13:00:28 +0900

    refactor: コンテンツ部分の横幅調整

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import { DescriptionCard } from '@/app/components/DescriptionCard';
 
 export default function AboutPage() {
   return (
-    <main className='bg-white flex flex-col gap-8 pt-8'>
+    <main className='bg-white flex flex-col gap-8 py-8'>
       <h1 className='text-2xl font-bold text-primary px-4'>SAKAGUCHIのこだわり</h1>
       <TitleAndContents title="豊富なパンの種類">
         <p>パン工房SAKAGUCHIは、その名の通り「工房」が店舗と一緒になっています。毎日100種類以上のパンを焼き上げており、準備が出来次第すぐに店頭に並びます。</p>
