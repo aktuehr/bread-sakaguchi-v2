b9b87bceff0e8a5f64addc327b4f1c7c1b28935e
Akito UEHARA <akt.uehr@gmail.com>
2025-03-15 14:35:26 +0900

    fix: フッタの文字色を修正

--- src/app/features/Footer.tsx
+++ src/app/features/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary text-primary text-white py-2">
+    <footer className="bg-secondary text-primary py-2">
       <div className="container mx-auto">
         <p className="text-center">© 2019- パン工房SAKAGUCHI</p>
       </div>
